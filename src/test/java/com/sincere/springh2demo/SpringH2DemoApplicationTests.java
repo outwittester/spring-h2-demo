6e9eb70ca13d0ff5bd49ed88034d9d74b4b072a4
xiong xikai <xikaixiong1024@gmail.com>
2022-11-15 11:10:26 -0500

    spring-h2-demo init

---
+++ src/test/java/com/sincere/springh2demo/SpringH2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sincere.springh2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringH2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
